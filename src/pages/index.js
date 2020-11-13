2aa4465faacd605c2add6b1a838b7a6b5cf6b6fd
natatat <natuduwela@gmail.com>
2020-11-13 14:16:03 -0800

    homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,25 @@
 import React from "react"
 import Footer from "../components/Footer"
+import styles from "./index.module.scss"
 
 export default function Home() {
   return (
     <div>
       <section className="scratched-in">
-        <div className="black-bg"></div>
+        <div className={styles.blackBg}></div>
         <canvas className="in"></canvas>
       </section>
 
       <section className="scratched-out">
-        <div className="content">
-          <div className="about">
-            I'm Natalie – a designer, developer, and type-enthusiast based in
-            California.
-          </div>
+        <div className={styles.content}>
+          I'm Natalie – a designer, developer, and type-enthusiast based in
+          California.
         </div>
         <canvas className="out"></canvas>
       </section>
 
-      <div className="cursor"></div>
-      <div className="cursor-text">click & drag</div>
+      <div className={styles.cursor}></div>
+      <div className={styles.cursorText}>click & drag</div>
 
       <Footer />
     </div>
