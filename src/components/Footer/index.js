45d3a1a1df9ccbed2033f1d6428aeb46fda8030b
natatat <natuduwela@gmail.com>
2020-11-13 10:54:00 -0800

    update Footer component + styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,5 +2,97 @@ import React from "react"
 import styles from "./footer.module.scss"
 
 export default function Footer() {
-  return <div className={styles.footer}>fooooter</div>
+  return (
+    <div className={styles.footer}>
+      <ul className={styles.nav}>
+        <li className={styles.navLeft}>
+          <a
+            href="https://www.notion.so/uduwela/17f2affc87ea43a58efe52113d34f94c?v=9d5f2efbf1dc4b4a92ade63d3541f75b"
+            target="_blank"
+            rel="noreferrer"
+          >
+            portfolio
+          </a>
+        </li>
+        <li className={styles.navLeft}>
+          <a
+            href="http://nat.uduwela.com/natalie-uduwela-resume.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            resume
+          </a>
+        </li>
+        <li className={styles.navRight}>
+          <a href="mailto:nat@uduwela.com" target="_blank" rel="noreferrer">
+            email
+          </a>
+        </li>
+        <li className={styles.navRight}>
+          <a
+            href="http://instagram.com/abc.nat"
+            target="_blank"
+            rel="noreferrer"
+          >
+            instagram
+          </a>
+        </li>
+        <li className={styles.navRight}>
+          <a
+            href="https://www.linkedin.com/in/natalieuduwela/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedin
+          </a>
+        </li>
+      </ul>
+
+      <div class="chevron up"></div>
+      <ul className={`${styles.mobileNav} hide`}>
+        <li class="chevron down"></li>
+        <li class="left">
+          <a
+            href="https://www.notion.so/uduwela/17f2affc87ea43a58efe52113d34f94c?v=9d5f2efbf1dc4b4a92ade63d3541f75b"
+            target="_blank"
+            rel="noreferrer"
+          >
+            portfolio
+          </a>
+        </li>
+        <li class="left">
+          <a
+            href="http://nat.uduwela.com/natalie-uduwela-resume-2020.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            resume
+          </a>
+        </li>
+        <li class="left">
+          <a
+            href="https://www.linkedin.com/in/natalieuduwela/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedin
+          </a>
+        </li>
+        <li class="left">
+          <a
+            href="http://instagram.com/abc.nat"
+            target="_blank"
+            rel="noreferrer"
+          >
+            instagram
+          </a>
+        </li>
+        <li class="left">
+          <a href="mailto:nat@uduwela.com" target="_blank" rel="noreferrer">
+            email
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
@@ -22,100 +22,7 @@ export default function Home() {
       <div class="cursor"></div>
       <div class="cursor-text">click & drag</div>
 
-      <div class="footer">
-        <ul class="nav">
-          <li class="left">
-            <a
-              href="https://www.notion.so/uduwela/17f2affc87ea43a58efe52113d34f94c?v=9d5f2efbf1dc4b4a92ade63d3541f75b"
-              target="_blank"
-              rel="noreferrer"
-            >
-              portfolio
-            </a>
-          </li>
-          <li class="left">
-            <a
-              href="http://nat.uduwela.com/natalie-uduwela-resume-2020.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              resume
-            </a>
-          </li>
-          <li class="left">
-            <Link to="/fun/">fun</Link>
-          </li>
-          <li class="right">
-            <a href="mailto:nat@uduwela.com" target="_blank" rel="noreferrer">
-              email
-            </a>
-          </li>
-          <li class="right">
-            <a
-              href="http://instagram.com/abc.nat"
-              target="_blank"
-              rel="noreferrer"
-            >
-              instagram
-            </a>
-          </li>
-          <li class="right">
-            <a
-              href="https://www.linkedin.com/in/natalieuduwela/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              linkedin
-            </a>
-          </li>
-        </ul>
-
-        <div class="chevron up"></div>
-        <ul class="nav mobile hide">
-          <li class="chevron down"></li>
-          <li class="left">
-            <a
-              href="https://www.notion.so/uduwela/17f2affc87ea43a58efe52113d34f94c?v=9d5f2efbf1dc4b4a92ade63d3541f75b"
-              target="_blank"
-              rel="noreferrer"
-            >
-              portfolio
-            </a>
-          </li>
-          <li class="left">
-            <a
-              href="http://nat.uduwela.com/natalie-uduwela-resume-2020.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              resume
-            </a>
-          </li>
-          <li class="left">
-            <a
-              href="https://www.linkedin.com/in/natalieuduwela/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              linkedin
-            </a>
-          </li>
-          <li class="left">
-            <a
-              href="http://instagram.com/abc.nat"
-              target="_blank"
-              rel="noreferrer"
-            >
-              instagram
-            </a>
-          </li>
-          <li class="left">
-            <a href="mailto:nat@uduwela.com" target="_blank" rel="noreferrer">
-              email
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Footer />
     </div>
   )
 }
