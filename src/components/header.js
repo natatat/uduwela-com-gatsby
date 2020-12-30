0c50b8aa198cf999590d2c32500f233ebb188ed1
natatat <natuduwela@gmail.com>
2020-12-30 12:46:15 -0800

    cleanup
    - TODO later: add 404 page

--- src/components/header.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function Header({ text }) {
-  return <h1>{text}</h1>
-}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,9 @@
 import React from "react"
-import Header from "../components/header"
 
 export default function _404() {
   return (
     <div>
-      <Header text="404" />
+      <h1>404</h1>
     </div>
   )
 }
